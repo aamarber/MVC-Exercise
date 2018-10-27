d2b12a01c12e2e3480dd2ed87ad101f58baed8df
melocotoneroh <aamarber@gmail.com>
2018-10-27 19:36:12 +0200

    Add project files.

---
+++ ASPNETMVC/Controllers/CoursesController.cs
@@ -0,0 +1,67 @@
+﻿using CoursesManagement.Domain;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace ASPNETMVC.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private static IList<Student> students = new List<Student>
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "David"
+            },
+            new Student
+            {
+                Id = 2,
+                Name = "Pablo"
+            },
+            new Student
+            {
+                Id = 3,
+                Name = "Moisés"
+            },
+            new Student
+            {
+                Id = 4,
+                Name = "Sergio"
+            }
+        };
+
+        private static Teacher teacher = new Teacher
+        {
+            Name = "Aarón",
+            Id = 1
+        };
+
+        private IList<Course> courses = new List<Course>
+        {
+            new Course
+            {
+                Name = "Formacion Amaris",
+                Students = students,
+                Teacher = teacher
+            },
+            new Course
+            {
+                Name = "Formacion Customerville",
+                Students = students,
+                Teacher = teacher
+            },
+            new Course
+            {
+                Name = "Formacion Inventada",
+                Students = students,
+                Teacher = teacher
+            },
+        };
+
+        // GET: Courses
+        public ActionResult Index()
+        {
+            return View(courses);
+        }
+    }
+}
---
+++ SampleApp/Sample.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp
+{
+    class Sample
+    {
+        public string Hola { get; set; }
+    }
+}
